339b33358528c0b7e671cefeb0459ae2755c9ec9
dimonnorin <dimon.norin241@gmail.com>
2018-09-04 23:11:15 +0700

    Lab0

--- 0Lab/source.cpp
+++ 0Lab/source.cpp
@@ -8,13 +8,12 @@ int main(int argc, char** argv)
 	
 	std::cout << Module1::getMyName() << "\n";
 	std::cout << Module2::getMyName() << "\n";
-
 	using namespace Module1;
 	std::cout << getMyName() << "\n"; // (A)
 	std::cout << Module2::getMyName() << "\n";
 
-	//using namespace Module2; // (B)
-	//std::cout << getMyName() << "\n"; // COMPILATION ERROR (C)
+	using namespace Module2; // (B)
+	std::cout << getMyName() << "\n"; // COMPILATION ERROR (C)
 
 	using Module2::getMyName;
 	std::cout << getMyName() << "\n"; // (D)

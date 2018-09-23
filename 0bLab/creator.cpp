a207728df59560005b686249e30eeffd36d27d18
dimonnorin <dimon.norin241@gmail.com>
2018-09-23 21:24:30 +0700

    refactoring

--- 0bLab/creator.cpp
+++ 0bLab/creator.cpp
@@ -1,15 +1,24 @@
 #include"creator.h"
+#include<algorithm>
+#include<vector>
 
 
 namespace Creator {
 
-	void fill_file(std::ofstream& output_file, std::map<std::string, int>& words_data) {
+	bool comp(std::pair<std::string , int> a , std::pair<std::string , int> b){
+		return (a.second > b.second);
+	}
 
+	void fill_file(std::ofstream& output_file, std::map<std::string, int>& words_data) {
+		std::vector<std::pair<std::string , int>> out_data(words_data.begin() , words_data.end());
+
+		std::sort(out_data.begin() , out_data.end() , comp);
+	
 		char separator = ',';
 		double words_count = words_data.size();
-		output_file << "Word" << separator << "Count" << separator << "Percentages" << std::endl;
+		utput_file << "Word" << separator << "Count" << separator << "Percentages" << std::endl;
 		
-		for (auto data : words_data) {
+		for (auto data : out_data) {
 			std::string out_word = data.first;
 			output_file << out_word << separator << data.second << separator << data.second / words_count * 100 << std::endl;
 		}
--- 0bLab/source.cpp
+++ 0bLab/source.cpp
@@ -1,3 +1,4 @@
+
 #include<fstream>
 #include<cassert>
 #include<exception>
